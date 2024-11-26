e201028f168e9ca703b1b0a48a8c0d7251b8541a
Estel <jiankangzhao7@gmail.com>
2024-11-26 12:40:04 +0800

    complete simple function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 // index.js
-console.log(greeting);
+const dropDownBtn = document.querySelector(".dropDownButton");
+const dropDownContent = document.querySelector(".dropDownContent");
 
+dropDownContent.style.display = "none";
+dropDownBtn.addEventListener("mouseover", () => {
+  dropDownBtn.style.color = "orange";
+  dropDownContent.style.display = "";
+});
+
+dropDownContent.addEventListener("mouseleave", () => {
+  dropDownBtn.style.color = "black";
+  dropDownContent.style.display = "none";
+});
